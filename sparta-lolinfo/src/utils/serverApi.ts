15e886e0c0248d635899aeba0be7118e65942ff5
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-03-17 05:14:08 +0900

    refactor: 설정 파일 분리

--- sparta-lolinfo/src/utils/serverApi.ts
+++ sparta-lolinfo/src/utils/serverApi.ts
@@ -1,12 +1,11 @@
 "use server";
 
 import { Champion, ChampionDetail } from "@/types/Champion";
-
-const BASE_URL = process.env.DDRAGON_BASE_URL;
+import { DDRAGON_BASE_URL } from "@/constants/config";
 
 // 최신 버전 가져오기 -> 버전이 자주 바뀌지는 않으므로 캐싱 데이터를 사용하는 최적화 필요
 async function getLatestVersion(): Promise<string> {
-  const res = await fetch(`${BASE_URL}/api/versions.json`);
+  const res = await fetch(`${DDRAGON_BASE_URL}/api/versions.json`);
   if (!res.ok) throw new Error("버전 정보를 가져오지 못했습니다.");
   const versions = await res.json();
   return versions[0];
@@ -16,7 +15,7 @@ async function getLatestVersion(): Promise<string> {
 export async function fetchChampionList(): Promise<Champion[]> {
   const version = await getLatestVersion();
   const res = await fetch(
-    `${BASE_URL}/cdn/${version}/data/ko_KR/champion.json`
+    `${DDRAGON_BASE_URL}/cdn/${version}/data/ko_KR/champion.json`
   );
   if (!res.ok) throw new Error("챔피언 목록을 가져오지 못했습니다.");
   const data = await res.json();
@@ -27,7 +26,7 @@ export async function fetchChampionList(): Promise<Champion[]> {
 export async function fetchChampionDetail(id: string): Promise<ChampionDetail> {
   const version = await getLatestVersion();
   const res = await fetch(
-    `${BASE_URL}/cdn/${version}/data/ko_KR/champion/${id}.json`
+    `${DDRAGON_BASE_URL}/cdn/${version}/data/ko_KR/champion/${id}.json`
   );
   if (!res.ok) throw new Error("챔피언 상세 정보를 가져오지 못했습니다.");
   const data = await res.json();
