a7e529f4dcfd75a245e3bd3b276ef094e61641fe
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-03-17 05:00:55 +0900

    feat: 데이터 fetching 함수 작성

---
+++ sparta-lolinfo/src/utils/serverApi.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { Champion, ChampionDetail } from "@/types/Champion";
+
+const BASE_URL = process.env.DDRAGON_BASE_URL;
+
+// 최신 버전 가져오기 -> 버전이 자주 바뀌지는 않으므로 캐싱 데이터를 사용하는 최적화 필요
+async function getLatestVersion(): Promise<string> {
+  const res = await fetch(`${BASE_URL}/api/versions.json`);
+  if (!res.ok) throw new Error("버전 정보를 가져오지 못했습니다.");
+  const versions = await res.json();
+  return versions[0];
+}
+
+// 챔피언 목록 가져오기
+export async function fetchChampionList(): Promise<Champion[]> {
+  const version = await getLatestVersion();
+  const res = await fetch(
+    `${BASE_URL}/cdn/${version}/data/ko_KR/champion.json`
+  );
+  if (!res.ok) throw new Error("챔피언 목록을 가져오지 못했습니다.");
+  const data = await res.json();
+  return Object.values(data.data);
+}
+
+// 챔피언 상세 정보 가져오기
+export async function fetchChampionDetail(id: string): Promise<ChampionDetail> {
+  const version = await getLatestVersion();
+  const res = await fetch(
+    `${BASE_URL}/cdn/${version}/data/ko_KR/champion/${id}.json`
+  );
+  if (!res.ok) throw new Error("챔피언 상세 정보를 가져오지 못했습니다.");
+  const data = await res.json();
+  return Object.values(data.data)[0] as ChampionDetail;
+}
