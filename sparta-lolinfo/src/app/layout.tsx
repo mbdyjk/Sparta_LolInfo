1530f872f61e09dcf3b16f8279cad282bca0317e
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-03-19 11:14:17 +0900

    feat: 라이트 모드, 다크 모드 추가

--- sparta-lolinfo/src/app/layout.tsx
+++ sparta-lolinfo/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Link from "next/link";
 import ClientProvider from "@/components/ClientProvider";
+import Header from "@/Header";
 
 export const metadata: Metadata = {
   title: "Sparta LolInfo",
@@ -15,37 +15,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>
-        <header className="bg-gray-800 text-white p-4">
-          <nav className="container mx-auto flex justify-center">
-            <div className="flex justify-between w-full">
-              <Link
-                href="/"
-                className="flex font-bold justify-center flex-1 hover:text-gray-300"
-              >
-                홈
-              </Link>
-              <Link
-                href="/champions"
-                className="flex font-bold justify-center flex-1 hover:text-gray-300"
-              >
-                챔피언 목록
-              </Link>
-              <Link
-                href="/items"
-                className="flex font-bold justify-center flex-1 hover:text-gray-300"
-              >
-                아이템 목록
-              </Link>
-              <Link
-                href="/rotation"
-                className="flex font-bold justify-center flex-1 hover:text-gray-300"
-              >
-                챔피언 로테이션
-              </Link>
-            </div>
-          </nav>
-        </header>
+      <body className="bg-white dark:bg-gray-800 text-black dark:text-white">
+        <Header />
         <ClientProvider>
           <main>{children}</main>
         </ClientProvider>
