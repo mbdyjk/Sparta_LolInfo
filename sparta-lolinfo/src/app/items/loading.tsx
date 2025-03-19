e9589831179f8213fb59cde9667b3883d1e76933
mbdyjk@gmail.com <mbdyjk@gmail.com>
2025-03-19 11:05:03 +0900

    feat: CSR 페이지 제외 Loading UI 추가

---
+++ sparta-lolinfo/src/app/items/loading.tsx
@@ -0,0 +1,7 @@
+export default function ItemsLoading() {
+  return (
+    <div className="container mx-auto p-4 text-center">
+      <p className="text-xl">아이템 목록을 불러오는 중...</p>
+    </div>
+  );
+}
